5a1263e3ceded1980c9fa0839e881539ee62740a
Abdulaziz <abdulazizmirabdullayev.20@gmail.com>
2023-05-05 13:38:20 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.Api/Models/Foundations/Guests/Exceptions/FailedGuestStorageException.cs
@@ -0,0 +1,25 @@
+﻿//================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+//================================
+
+using Microsoft.Data.SqlClient;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Guests.Exceptions
+{
+    public class FailedGuestStorageException:Xeption
+    {
+        private SqlException sqlException;
+
+        public FailedGuestStorageException(Xeption innerException)
+            :base(message: "Failed guest storage error occurred, contact support",
+                 innerException)
+        {}
+
+        public FailedGuestStorageException(SqlException sqlException)
+        {
+            this.sqlException = sqlException;
+        }
+    }
+}
