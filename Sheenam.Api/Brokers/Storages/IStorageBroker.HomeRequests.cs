0c96df1877dded703762b00e5b05351fed1f44e9
Abdulaziz <abdulazizmirabdullayev.20@gmail.com>
2023-05-11 11:03:35 +0500

    CODE RUB: Fix the Error

--- Sheenam.Api/Brokers/Storages/IStorageBroker.HomeRequests.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.HomeRequests.cs
@@ -1,6 +1,15 @@
-﻿namespace Sheenam.Api.Brokers.Storages
+﻿//================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To Find Comfort and Peace
+//================================
+
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+
+namespace Sheenam.Api.Brokers.Storages
 {
-    public interface IStorageBroker
+    public partial interface IStorageBroker
     {
+        ValueTask<HomeRequest> InsertHomeRequestAsync(HomeRequest homeRequest);
     }
 }
